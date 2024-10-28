94d20d7af6a1a66684f6ce1465f655c0010dc97d
sunyoung <skditjsdud35@naver.com>
2024-10-29 07:24:02 +0900

    feat: react query Provider Hook 구현

---
+++ src/hooks/common/useProviders.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from "react-query"
+
+export default function Providers({ children }: React.PropsWithChildren) {
+
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        staleTime: 60 * 1000,
+                    },
+                },
+            }),
+    )
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
