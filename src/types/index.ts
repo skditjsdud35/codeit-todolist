091b87d67157d2974ee8bef332ddebba8592ecad
sunyoung <skditjsdud35@naver.com>
2024-10-29 07:16:33 +0900

    feat: type 설정

---
+++ src/types/index.ts
@@ -0,0 +1,30 @@
+// Item type
+export interface GetItem {
+    isCompleted: boolean;
+    name: string;
+    id: number;
+}
+
+// Item 상세 type
+export interface GetItemDetail extends GetItem {
+    imageUrl?: string | null;
+    memo?: string;
+    tenantId?: string;
+}
+
+// Item 추가 type
+export interface PostItem {
+    name: string;
+    memo?: string | null;
+    imageUrl?: string | null;
+    isCompleted: boolean;
+    id: number;
+}
+
+// Item 수정 type
+export interface PatchItem {
+    name?: string;
+    memo?: string | null;
+    imageUrl?: string | null;
+    isCompleted: boolean;
+}
